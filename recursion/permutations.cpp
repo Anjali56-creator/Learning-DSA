21d87627db934af4002f9bd6aeb887aec5c098a0
Anjali Chaurasiya <anjalichaurasiya2348@gmail.com>
2026-01-17 22:31:00 +0530

    permutation

---
+++ recursion/permutations.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void permut(int arr[],vector<vector<int>>& ans,vector<int> &temp,vector<bool> &visited){
+    if(visited.size()==temp.size()){
+        ans.push_back(temp);
+        return;
+    }
+    for(int i=0;i<visited.size();i++){
+        if(visited[i]==0){
+        visited[i]=1;
+        temp.push_back(arr[i]);
+        permut(arr,ans,temp,visited);
+        visited[i]=0;
+        temp.pop_back();
+        }
+    }
+}
+int main(){
+    int n,arr[20];
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    vector<vector<int>>ans;
+    vector<int>temp;
+    vector<bool>visited(n,0);
+    permut(arr,ans,temp,visited);
+    for(int i=0;i<ans.size();i++){
+        for(int j=0;j<ans[i].size();j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
