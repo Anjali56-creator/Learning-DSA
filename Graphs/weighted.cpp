cc8665984bb12afa2f4439353ece4fc1134fd7b9
Anjali56-creator <anjalichaurasiya2348@gmail.com>
2026-06-01 22:11:14 +0530

    graphs

---
+++ Graphs/weighted.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int vertex,edges;
+    cout<<"Enter number of vertex and edges: ";
+    cin>>vertex>>edges;
+    vector<vector<int>>AdjMat(vertex,vector<int>(vertex,0));
+    int u,v,weight;
+    for(int i=0;i<edges;i++){
+        cin>>u>>v>>weight;
+        AdjMat[u][v]=weight;
+        AdjMat[v][u]=weight;
+    }
+    cout<<"Adjacency Matrix: "<<endl;
+    for(int i=0;i<vertex;i++){
+        for(int j=0;j<vertex;j++){
+            cout<<AdjMat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+
+}
